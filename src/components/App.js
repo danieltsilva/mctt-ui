addb5d3ad225cfc01110a10d8bf8f8fecf26b95e
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-05-16 23:24:10 +1000

    Basic TextArea for input sample texts

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { 
+    Form,
+    TextArea,
+    Container,
+    Divider 
+    } from 'semantic-ui-react';
 
 class App extends React.Component {
   render() {
     return (
-      <div> TESTES </div>
+        <div>
+            <Container textAlign='left'>
+                Text to train your Markov Chain
+                <Divider />
+                <Form>
+                    <TextArea placeholder='Past your text here' />
+                </Form>
+            </Container>
+        </div>
     );
   }
 }
