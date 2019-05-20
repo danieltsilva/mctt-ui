b55275cc821e75d963dba2055f1191aa2c688da9
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-05-20 16:32:26 +1000

    Axios to assyncronous requests to framework

--- src/apis/mcttFramework.js
+++ src/apis/mcttFramework.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     // Docker service framework
-    //baseURL: 'http://mctt-framework:8080'
-    
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://mctt-framework:8080'
+
+    //baseURL: 'http://localhost:8080'
 });
